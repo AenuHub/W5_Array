d4c560c0127a14d9c2bbf6daf32811f7c687757c
AenuHub <jesuskane92@gmail.com>
2024-10-28 14:38:18 +0300

    update resize method

--- W5_Array/Program.cs
+++ W5_Array/Program.cs
@@ -16,19 +16,15 @@
         Console.Write("Give another number for 11th element: ");
         int input = int.Parse(Console.ReadLine());
 
-        // creating new array as old one cannot be expanded, and add input as last element
-        int[] newNumbers = new int[numbers.Length + 1];
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            newNumbers[i] = numbers[i];
-        }
-        newNumbers[numbers.Length] = input;
+        // resizing the array, and add input as last element
+        Array.Resize(ref numbers, numbers.Length + 1);
+        numbers[numbers.Length - 1] = input;
         
         // sort the array in descending order
-        Array.Sort(newNumbers);
-        for (int i = newNumbers.Length - 1; i >= 0; i--)
+        Array.Sort(numbers);
+        for (int i = numbers.Length - 1; i >= 0; i--)
         {
-            Console.WriteLine(newNumbers[i]);
+            Console.WriteLine(numbers[i]);
         }
 
         Console.ReadKey();
